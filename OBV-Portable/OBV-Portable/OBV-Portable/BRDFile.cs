b93374a2602464f56d37345f75c02acff6381083
willthehuman <williamcperrier@gmail.com>
2022-11-24 17:50:00 -0500

    Load and verify .brd file

---
+++ OBV-Portable/OBV-Portable/OBV-Portable/BRDFile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+using System.Text;
+
+namespace OBV
+{
+    internal class BRDFile
+    {
+        static readonly Byte[] BRD_FILE_HEADER = { 0x23, 0xe2, 0x63, 0x28 };
+        public BRDFile(Stream buffer)
+        {
+            var fileBuffer = new StreamReader(buffer, Encoding.UTF8);
+            fileBuffer.BaseStream.Position = 0;
+
+            // Read the first 4 bytes of the file
+            Byte[] fileHeader = new Byte[4];
+            fileBuffer.BaseStream.Read(fileHeader, 0, 4);
+
+            // Check if the file is a valid .brd file by checking the header
+            for (int i = 0; i < 4; i++)
+            {
+                if (fileHeader[i] != BRD_FILE_HEADER[i])
+                {
+                    throw new Exception("Invalid .brd file");
+                }
+            }
+
+            
+        }
+
+        public string FileName { get; internal set; }
+        public string FilePath { get; internal set; }
+        public string FileContent { get; internal set; }
+    }
+}
--- OBV-Portable/OBV-Portable/OBV-Portable/MainPage.xaml.cs
+++ OBV-Portable/OBV-Portable/OBV-Portable/MainPage.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using OBV;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace OBV_Portable
@@ -13,6 +16,36 @@ namespace OBV_Portable
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void BtnLoadFile_Clicked(object sender, EventArgs e)
+        {
+            var pickOptions = new PickOptions
+            {
+                PickerTitle = "Select a board file",
+                FileTypes = null
+            };
+            // Open a file picker to select a file
+            var file = await FilePicker.PickAsync(pickOptions);
+
+            if (file != null && file.FileName.EndsWith(".brd"))
+            {
+                await DisplayAlert("Success", "The file " + file.FileName + " will be loaded.", "OK");
+                LoadFile(file);
+            }
+            else
+            {
+                await DisplayAlert("Error", "Please select a valid .brd file", "OK");
+            }
+        }
+
+        private async void LoadFile(FileResult file)
+        {
+            // Get the file stream
+            var stream = await file.OpenReadAsync();
+
+            // Load the file as a list of characters
+            var brdFile = new BRDFile(stream);
         }
     }
 }
