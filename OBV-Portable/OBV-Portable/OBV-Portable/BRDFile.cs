d10f56e34d78da8d962b0333b5c5497d2ed2fd1e
willthehuman <williamcperrier@gmail.com>
2022-11-25 15:09:25 -0500

    decode brd file started

--- OBV-Portable/OBV-Portable/OBV-Portable/BRDFile.cs
+++ OBV-Portable/OBV-Portable/OBV-Portable/BRDFile.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
 using System.Numerics;
 using System.Text;
+using Xamarin.Forms.Shapes;
 
 namespace OBV
 {
@@ -11,12 +13,11 @@ namespace OBV
         static readonly Byte[] BRD_FILE_HEADER = { 0x23, 0xe2, 0x63, 0x28 };
         public BRDFile(Stream buffer)
         {
-            var fileBuffer = new StreamReader(buffer, Encoding.UTF8);
-            fileBuffer.BaseStream.Position = 0;
+            buffer.Position = 0;
 
             // Read the first 4 bytes of the file
-            Byte[] fileHeader = new Byte[4];
-            fileBuffer.BaseStream.Read(fileHeader, 0, 4);
+            Byte[] fileHeader = new Byte[buffer.Length];
+            buffer.Read(fileHeader, 0, (int)buffer.Length);
 
             // Check if the file is a valid .brd file by checking the header
             for (int i = 0; i < 4; i++)
@@ -27,11 +28,78 @@ namespace OBV
                 }
             }
 
-            
-        }
+            var memStream = new MemoryStream();
 
-        public string FileName { get; internal set; }
-        public string FilePath { get; internal set; }
-        public string FileContent { get; internal set; }
+            // decode the file
+            foreach (byte b in fileHeader)
+            {
+                // convert byte to char
+                char x = (char)b;
+                if (x == '\r' || x == '\n' || x == 0)
+                {
+                    // ignore
+                }
+                else
+                {
+                    // decode
+                    int c = x;
+                    x = (char)~(((c >> 6) & 3) | (c << 2));
+                    // write x to stream
+                    Console.Write(x);
+                    memStream.WriteByte((byte)x);
+                }
+            }
+
+            memStream.Position = 0;
+            var reader = new StreamReader(memStream);
+
+            // Read the memStream line by line
+            String line;            
+            int currentBlock = 0;
+            while ((line = reader.ReadLine()) != null)
+            {
+                // split on multiple spaces
+                String[] lineSplit = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                // remove what's after the colon
+                var test = lineSplit[0].Split(':')[0];
+
+                if (test == "str_length")
+                {
+                    currentBlock = 1;
+                    continue;
+                }
+
+                if (test == "var_data")
+                {
+                    currentBlock = 2;
+                    continue;
+                }
+
+                if (test == "Format" || test == "format")
+                {
+                    currentBlock = 3;
+                    continue;
+                }
+
+                if (test == "Parts" || test == "Pins1")
+                {
+                    currentBlock = 4;
+                    continue;
+                }
+
+                if (test == "Pins" || test == "Pins2")
+                {
+                    currentBlock = 5;
+                    continue;
+                }
+
+                if (test == "Nails")
+                {
+                    currentBlock = 6;
+                    continue;
+                }
+            }
+        }
     }
 }
